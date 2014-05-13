1f44b193be41649722c598b45df4e74fb2accc7c
danielwells <danielwells.usnret@gmail.com>
2014-05-13 13:26:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socketchat::Application.config.secret_token = 'bb98cfb66d893dd19e9dfc758c88b7adf4381ecab5c7698e9d62ecd36792974a70abf9c426dbad724945ac3773fc57958dffd53e2ba6a41e7b82651aa7b1ea5e'
